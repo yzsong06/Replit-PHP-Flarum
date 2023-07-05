7e9f9eb6966f91de959c34b369add3c5d707b752
一只鬆 <yzsong06@outlook.com>
2023-07-05 14:24:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 echo Replit_Flarum数据库安装脚本
-echo Replit_Flarum Install the database
+echo Replit_Flarum Install the Database
 echo By The 一只鬆（Yzsong06）
 echo GitHub开源地址：https://github.com/yzsong06/Replit-PHP-Flarum
 echo GitHub open source address：https://github.com/yzsong06/Replit-PHP-Flarum
-
+wget -O data.tar.gz https://github.com/yzsong06/Replit-PHP-Flarum/raw/main/data.tar.gz
 tar -zxvf data.tar.gz
+rm -rf data.tar.gz
+echo 写入数据库数据
+echo Install Database
 cat <<EOF > $HOME/$REPL_SLUG/data/my.cnf
 [server]
 datadir=$HOME/$REPL_SLUG/data
