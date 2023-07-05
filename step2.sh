c909de18ee0d5f8400ae7318bd0034a82cfef35b
一只鬆 <yzsong06@outlook.com>
2023-07-05 08:24:30 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,1 +1,11 @@
+export MYSQL_HOME=$HOME/$REPL_SLUG/data
 
+mkdir public
+cd public
+nix-env -iA nixpkgs.php80Packages.composer
+composer create-project flarum/flarum .
+composer require flarum-lang/chinese-simplified
+echo "恭喜搭建完成"
+echo "点击Run立即运行项目"
+rm -rf README.md
+rm -rf .git
