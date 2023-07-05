5f969049b9715005d8c5e710a049f268392726c5
一只鬆 <yzsong06@outlook.com>
2023-07-05 08:23:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,14 +10,3 @@ innodb-log-file-size=8M
 innodb_temp_data_file_path=ibtmp1:1M:autoextend:max:8M
 lower_case_table_names=1
 EOF
-
-export MYSQL_HOME=$HOME/$REPL_SLUG/data
-
-mkdir public
-cd public
-nix-env -iA nixpkgs.php80Packages.composer
-composer create-project flarum/flarum .
-composer require flarum-lang/chinese-simplified
-echo "恭喜搭建完成"
-echo "点击Run立即运行项目"
-rm -rf README.md && rm -rf .git
